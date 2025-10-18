90c73546d55d55e804f3033c189343a4ec662eca
Cristobal Carreno <cristblcarreo.f@gmail.com>
2025-10-17 21:49:05 -0300

    add sound and faces to ev3 movement

--- src/runner/engine/prims.py
+++ src/runner/engine/prims.py
@@ -4,36 +4,73 @@ from textwrap import dedent, indent
 
 def start_program():
     imports = """#!/usr/bin/env pybricks-micropython
+import _thread
+from pybricks.tools import wait
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, ColorSensor
 from pybricks.parameters import Port, Color
 from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+def play_sound(soundfile):
+    ev3.speaker.play_file(soundfile)
 
 ev3 = EV3Brick()
+ev3.speaker.play_file(SoundFile.READY)
 
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 color_sensor = ColorSensor(Port.S1)
+ev3.screen.load_image(ImageFile.THUMBS_UP)
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=75)
+ev3.speaker.play_file(SoundFile.MOTOR_START)
     """
     return imports
 
 def stop_program():
-    return ""
+    return """
+_thread.start_new_thread(play_sound, (SoundFile.MOTOR_STOP,))
+wait(1000)
+"""
 
 def move_fwd():
     inst = """
+_thread.start_new_thread(play_sound, (SoundFile.SPEED_UP,))
+ev3.screen.load_image(ImageFile.UP)
 robot.straight(100)
+wait(250)
     """
     return inst
 
 def move_bwd():
     inst = """
+_thread.start_new_thread(play_sound, (SoundFile.BACKING_ALERT,))
+ev3.screen.load_image(ImageFile.DOWN)
 robot.straight(-100)
+wait(1000)
     """
     return inst
 
+def turn_left():
+    inst = """
+_thread.start_new_thread(play_sound, (SoundFile.MOTOR_IDLE,))
+ev3.screen.load_image(ImageFile.MIDDLE_LEFT)
+robot.turn(-130)
+wait(250)
+"""
+    return inst
+
+def turn_right():
+    inst = """
+_thread.start_new_thread(play_sound, (SoundFile.MOTOR_IDLE,))
+ev3.screen.load_image(ImageFile.MIDDLE_RIGHT)
+robot.turn(130)
+wait(250)
+"""
+    return inst
+
+# COND BLOCKS
 def if_block(cond, body):
     indented = indent(body, "        ")
     inst = f"""
@@ -54,19 +91,6 @@ while {cond}:
     {indented_step}
     """
     return inst
-
-def turn_left():
-    inst = """
-robot.turn(-130)
-"""
-    return inst
-
-def turn_right():
-    inst = """
-robot.turn(130)
-"""
-    return inst
-
 
 def if_cond(cond, body):
     print(f"Condiciones: {cond}, cuerpo: {body}")
